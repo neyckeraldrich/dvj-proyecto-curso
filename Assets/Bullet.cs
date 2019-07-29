f512fd25ce3f7efc1d9e7bf1c6df1892f56d9aff
israel <israel_pasaca@icloud.com>
2019-07-29 06:01:19 -0500

    shoot

---
+++ Assets/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 20f;
+	
+	public Rigidbody2D rb;
+	
+
+	// Use this for initialization
+	void Start () {
+		rb.velocity = transform.right * speed;
+	}
+
+	
+}

4cd2d99314536c46c055f5113ce5899f0aaab699
Neycker Aguayo <neyckeraldrich@outlook.com>
2019-08-05 00:54:02 -0500

    Damage to enemies and to players, update lives counter, push box to exit

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int lives = 3;
+    public int health = 100;
+
+    public GameObject deathEffect;
+    public GameObject startPosition;
+
+    public TMPro.TextMeshProUGUI livesCounter;
+
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        Debug.Log("Player Health " + health);
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        if (deathEffect != null)
+        {
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+        }
+
+        // Move to starting position
+        this.transform.position = startPosition.transform.position;
+
+        // Reduce lives counter in UI
+        this.lives--;
+        livesCounter.text = lives.ToString();
+
+        // Reset Health
+        health = 100;
+    }
+}

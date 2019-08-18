5bcf6cbc3a85967f40deba8177a21f7b07785caf
dfsaigua <dfsaigua@espol.edu.ec>
2019-08-18 03:10:08 -0500

    movimientos y disparos de enemigos nivel 2

---
+++ nivel2/Assets/Scripts/disparo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disparo : MonoBehaviour
+{
+    public float bulletSpeed;
+    public float spawnTime;
+    public GameObject spawner;
+    public GameObject bulletPrefab;
+    private float counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter > spawnTime)
+        {
+            GameObject bullet = (GameObject)Instantiate(bulletPrefab, spawner.transform, true);
+            bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed, bullet.GetComponent<Rigidbody2D>().velocity.y);
+            counter = 0;
+        }
+    }
+}
+
+
+ 
---
+++ nivel2/Assets/Scripts/movimiento.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("right")) {
+            if (GetComponent<SpriteRenderer>().flipX == true) {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            transform.Translate(0.02f,0,0);
+        }
+        if (Input.GetKey("left")) {
+            if (GetComponent<SpriteRenderer>().flipX==false)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            transform.Translate(-0.02f,0,0);
+        }
+        
+
+    }
+}

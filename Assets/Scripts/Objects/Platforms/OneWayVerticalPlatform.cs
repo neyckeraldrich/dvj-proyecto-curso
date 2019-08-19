557d9016bd105f2130f73368dbb548a3bb6c911b
Neycker Aguayo <neyckeraldrich@outlook.com>
2019-08-18 19:06:05 -0500

    OneWayPlatform, audio

---
+++ Assets/Scripts/Objects/Platforms/OneWayVerticalPlatform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayVerticalPlatform : MonoBehaviour
+{
+
+    private PlatformEffector2D effector2D;
+    public float waitTime = 0.5f;
+
+    private float remainingWaitTime;
+
+    void Start()
+    {
+        effector2D = GetComponent<PlatformEffector2D>();
+        remainingWaitTime = waitTime;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonUp("Crouch"))
+        {
+            remainingWaitTime = waitTime;
+        }
+
+        if (Input.GetButton("Crouch"))
+        {
+            if (remainingWaitTime <= 0f)
+            {
+                effector2D.rotationalOffset = 180f;
+                remainingWaitTime = waitTime;
+            }
+            else
+            {
+                remainingWaitTime -= Time.deltaTime;
+            }
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            effector2D.rotationalOffset = 0f;
+        }
+
+    }
+}

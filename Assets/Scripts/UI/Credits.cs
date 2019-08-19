701a813b3775c2fa08cbae40311360ec0a54f247
Neycker Aguayo <neyckeraldrich@outlook.com>
2019-08-18 20:32:39 -0500

    Merged branch "Creditos"

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
